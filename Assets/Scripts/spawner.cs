74bca61eeae361ccd75b45eec808c900608f2859
Ptyraan <168381801+Ptyraan@users.noreply.github.com>
2026-02-15 18:46:17 -0500

    bugged? drawing don't show up on game window but do on secne window

---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        if (Mouse.current.leftButton.isPressed)
+        {
+            Instantiate(prefab, transform.position, transform.rotation);
+        }
+    }
+}
