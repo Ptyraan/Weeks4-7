02bc87c3b3e018123c611e8009c330b29fdb2c2d
pangta <pangta@OA-S141-01>
2026-02-18 11:35:30 -0500

    slightly not working as intended

---
+++ Assets/Deadly Dungeon/Mimic.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Mimic : MonoBehaviour
+{
+    public GameObject player;
+    public SpriteRenderer spriteRenderer;
+    public Sprite closed;
+    public Sprite open;
+    public bool trapped = false;
+    public int poise = 20;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trapped)
+        {
+            player.transform.position = transform.position;
+            if (Keyboard.current.anyKey.wasPressedThisFrame)
+            {
+                poise -= 1;
+            }
+            else 
+            { 
+                // do damage
+            }
+            if (poise <= 0)
+            {
+                trapped = false;
+                spriteRenderer.sprite = closed;
+            }
+        }
+        else
+        {
+            if (spriteRenderer.bounds.Contains(player.transform.position) == true
+             && Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                trapped = true;
+                spriteRenderer.sprite = open;
+            }
+        }
+    }
+}
